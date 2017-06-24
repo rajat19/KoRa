e4ab4fc186a45e41d744155e556e0aac2cbfa8c2
rajat19 <rajatsri94@gmail.com>
2017-06-24 13:45:13 +0530

    homepage

--- kora/urls.py
+++ kora/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+	url(r'^$', TemplateView.as_view(template_name='account/index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
     url(r'^books/', include('books.urls')),
