63c2c2318bad9629601e9166c360b56e04fb4b3e
Rajat <rajat.srivastava@smartprix.com>
2017-10-20 01:05:15 +0530

    updated books/templatetags

--- books/templatetags/book_extras.py
+++ books/templatetags/book_extras.py
@@ -2,10 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter('fieldtype')
-def fieldtype(field):
-	return field.field.widget.__class__.__name__
-
 @register.assignment_tag
 def define(val=None):
 	return val
