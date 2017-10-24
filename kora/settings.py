a17ebbec6665eb83ebe3968665f927b3369cd7c0
rajats-smpx <rajat.srivastava@smartprix.com>
2017-10-24 18:51:49 +0530

    moved db to postgres

--- kora/settings.py
+++ kora/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'kora.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kora',
+        'USER': 'root',
+        'PASSWORD': 'smartprix',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -143,7 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Run python manage.py collectstatic to sync static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
