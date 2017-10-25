a2659d0683e2119dbfdeafaf1418f69b1f412736
rajats-smpx <rajat.srivastava@smartprix.com>
2017-10-25 16:25:50 +0530

    added timestamps to all models

--- musique/models.py
+++ musique/models.py
@@ -12,19 +12,33 @@ class SongArtist(models.Model):
 	popularity = models.IntegerField(default=0)
 	photo = models.FileField(default='', null=True, blank=True)
 	photo_url = models.CharField(null=True, blank=True, max_length=250)
+	createdAt = models.DateTimeField(null=True, blank=True)
+	updatedAt = models.DateTimeField(auto_now = True, null=True, blank=True)
+	deletedAt = models.DateTimeField(null=True, blank=True)
 
 	def save(self, *args, **kwargs):
 		if not self.id:
 			self.slug = slugify(self.name)
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+
+		self.updatedAt = timezone.now()
 		super(SongArtist, self).save(*args, **kwargs)
 
 class Genre(models.Model):
 	slug = models.SlugField(max_length=40, unique=True)
 	name = models.CharField(max_length=100)
+	createdAt = models.DateTimeField(null=True, blank=True)
+	updatedAt = models.DateTimeField(auto_now = True, null=True, blank=True)
+	deletedAt = models.DateTimeField(null=True, blank=True)
 
 	def save(self, *args, **kwargs):
 		if not self.id:
 			self.slug = slugify(self.name)
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+
+		self.updatedAt = timezone.now()
 		super(Genre, self).save(*args, **kwargs)
 
 class SongAlbum(models.Model):
@@ -37,6 +51,9 @@ class SongAlbum(models.Model):
 	logo_url = models.CharField(null=True, blank=True, max_length=250)
 	popularity = models.IntegerField(default=0)
 	rating = models.IntegerField(default=0)
+	createdAt = models.DateTimeField(null=True, blank=True)
+	updatedAt = models.DateTimeField(auto_now = True, null=True, blank=True)
+	deletedAt = models.DateTimeField(null=True, blank=True)
 
 	def __str__(self):
 		return self.title
@@ -44,24 +61,37 @@ class SongAlbum(models.Model):
 	def save(self, *args, **kwargs):
 		if not self.id:
 			self.slug = slugify(self.name)
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+
+		self.updatedAt = timezone.now()
 		super(SongAlbum, self).save(*args, **kwargs)
 
 class Song(models.Model):
 	slug = models.SlugField(max_length=40, unique=True)
 	name = models.CharField(max_length=250)
+	artist = models.ForeignKey(SongArtist, on_delete=models.CASCADE)
 	album = models.ForeignKey(SongAlbum, on_delete=models.CASCADE)
 	genre = models.ManyToManyField(Genre)
 	language = models.CharField(max_length=40, null=True)
 	duration = models.CharField(max_length=10, null=True)
 	likes = models.IntegerField(default=0)
 	times_played = models.IntegerField(default=0)
+	createdAt = models.DateTimeField(null=True, blank=True)
+	updatedAt = models.DateTimeField(auto_now = True, null=True, blank=True)
+	deletedAt = models.DateTimeField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
 
 	def save(self, *args, **kwargs):
+		self.artist = self.album.artist
 		if not self.id:
 			self.slug = slugify(self.name)
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+			
+		self.updatedAt = timezone.now()
 		super(Song, self).save(*args, **kwargs)
 
 class SongUpload(models.Model):
@@ -69,10 +99,20 @@ class SongUpload(models.Model):
 	uploader = models.ForeignKey(User, on_delete=models.CASCADE)
 	file = models.FileField()
 	no_of_downloads = models.CharField(max_length=5, default='0')
+	createdAt = models.DateTimeField(null=True, blank=True)
+	updatedAt = models.DateTimeField(auto_now = True, null=True, blank=True)
+	deletedAt = models.DateTimeField(null=True, blank=True)
 
 	def extension(self):
 		fname, extension = os.path.splitext(self.file.name)
 		return extension
+
+	def save(self, *args, **kwargs):
+		if not self.createdAt:
+			self.createdAt = timezone.now()
+
+		self.updatedAt = timezone.now()
+		return super(SongUpload, self).save(*args, **kwargs)
 
 class SongReview(models.Model):
 	song = models.ForeignKey(Song, on_delete=models.CASCADE, null=True)
@@ -90,4 +130,4 @@ class SongReview(models.Model):
 			self.createdAt = timezone.now()
 
 		self.updatedAt = timezone.now()
-		return super(Review, self).save(*args, **kwargs)
+		return super(SongReview, self).save(*args, **kwargs)
