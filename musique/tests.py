1015379dc14a19694edb9eb40da17a5e345fff8c
rajats-smpx <rajat.srivastava@smartprix.com>
2017-10-26 20:05:15 +0530

    added some test for models

--- musique/tests.py
+++ musique/tests.py
@@ -1,6 +1,34 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from .models import SongArtist, SongAlbum, Song
 
-# Create your tests here.
+class SongArtistCase(TestCase):
+    def setUp(self):
+        SongArtist.objects.create(name = 'One Republic', country = 'USA')
+
+    def test_slug_generated(self):
+        """Check if slug generated successfully"""
+        artist = SongArtist.objects.get(name = 'One Republic')
+        self.assertEqual(artist.slug, 'one-republic')
+
+class SongAlbumCase(TestCase):
+    def setUp(self):    
+        SongArtist.objects.create(name = 'One Republic', country = 'USA')
+        artist = SongArtist.objects.get(name = 'One Republic')
+        artist.songalbum_set.create(
+            name = 'Native',
+            release_date = '2013-03-22'
+        )
+        
+    def test_slug_generated(self):
+        """Check if slug generated successfully"""
+        artist = SongAlbum.objects.get(name = 'Native')
+        self.assertEqual(artist.slug, 'native')
+
+# class SongCase(TestCase):
+#     def setUp(self):
+#         Song.objects.create(name = 'One Republic', country = 'USA')
+
+#     def test_slug_generated(self):
+#         """Check if slug generated successfully"""
+#         artist = Song.objects.get(name = 'One Republic')
+#         self.assertEqual(artist.slug, 'charlie-puth')
